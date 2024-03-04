d0e0c920a8a8d7298063aec0a9c7f83daf949ab0
FRC9289 <FRC9289@gmail.com>
2024-03-04 14:57:21 -0600

    added the timed drive class

---
+++ src/main/java/frc/robot/commands/ArmShootCommandTimed.java
@@ -0,0 +1,8 @@
+// package frc.robot.commands;
+
+// public class ArmShootCommandTimed extends ArmShootCommand {
+//     public ArmShootCommandTimed ()
+//     {
+
+//     }
+// }
---
+++ src/main/java/frc/robot/commands/DriveTimed.java
@@ -0,0 +1,58 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.ArmShooter;
+import frc.robot.subsystems.Chassis;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class DriveTimed extends Command{
+
+    Chassis _chassis;
+    double _seconds;
+    Timer timer = new Timer();
+    double _turn;
+    double _direction;
+
+    // idk if name needs to get changed
+    public DriveTimed(Chassis chassis, double turn, double direction,  double seconds)
+    {
+        _chassis = chassis;
+        _seconds = seconds;
+        _direction = direction;
+        _turn = turn;
+
+
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        while(!timer.hasElapsed(_seconds))
+        {
+            _chassis.Drive(_turn, _direction);
+        }
+        timer.stop();
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        _chassis.Drive(0, 0);
+        timer.stop();
+        System.out.println("interrupted");
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(_seconds);
+    }
+
+}
