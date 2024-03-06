4ba2efa7dcdd1b6bac53c6d14f7e1a623888da0b
FRC9289 <FRC9289@gmail.com>
2024-03-06 13:56:38 -0600

    Fix CommandConstants

--- src/main/java/frc/robot/commands/CommandConstants.java
+++ src/main/java/frc/robot/commands/CommandConstants.java
@@ -18,12 +18,12 @@ package frc.robot.commands;
  */
 public final class CommandConstants {
     // Gamepad Axises
-    public static final int GamepadAxisLeftStickX = 1;
-    public static final int GamepadAxisLeftStickY = 2;
-    public static final int GamepadAxisShoulder = 3;
-    public static final int GamepadAxisRightStickX = 4;
-    public static final int GamepadAxisRightStickY = 5;
-    public static final int GamepadAxisDpad = 6;
+    public static final int AxisLeftStickX = 0;
+    public static final int AxisLeftStickY = 1;
+    public static final int AxisLeftTrigger = 2;
+    public static final int AxisRightTrigger = 3;
+    public static final int AxisRightStickX = 4;
+    public static final int AxisRightStickY = 5;
 
     // Gamepad POVs
     public static final int PovUp = 0;
@@ -32,14 +32,14 @@ public final class CommandConstants {
     public static final int PovLeft = 270;
 
     // Gamepad buttons
-    public static final int GamepadButtonA = 1; // Bottom Button
-    public static final int GamepadButtonB = 2; // Right Button
-    public static final int GamepadButtonX = 3; // Left Button
-    public static final int GamepadButtonY = 4; // Top Button
-    public static final int GamepadButtonShoulderL = 5;
-    public static final int GamepadButtonShoulderR = 6;
-    public static final int GamepadButtonBack = 7;
-    public static final int GamepadButtonStart = 8;
-    public static final int GamepadButtonLeftStick = 9;
-    public static final int GamepadButtonRightStick = 10;
+    public static final int ButtonA = 1; // Bottom Button
+    public static final int ButtonB = 2; // Right Button
+    public static final int ButtonX = 3; // Left Button
+    public static final int ButtonY = 4; // Top Button
+    public static final int ButtonShoulderL = 5;
+    public static final int ButtonShoulderR = 6;
+    public static final int ButtonBack = 7;
+    public static final int ButtonStart = 8;
+    public static final int ButtonLeftStick = 9;
+    public static final int ButtonRightStick = 10;
 }
