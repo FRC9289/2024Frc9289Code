a34c6c930dead4e1d4b9a7b55fb0d96c4b8862d5
eva <photogonical@gmail.com>
2024-03-29 08:26:09 -0500

    hanger speed limit changes

--- src/main/java/frc/robot/subsystems/Hanger.java
+++ src/main/java/frc/robot/subsystems/Hanger.java
@@ -11,14 +11,14 @@ public class Hanger extends SubsystemBase{
     private final int moterCanID11 = 11;
     private final int moterCanID12 = 12;
 
-    private final int speedlimit = 100;
+    private final int speedlimit = 20;
 
     private CANSparkMax hangMotor11 = new CANSparkMax(moterCanID11, MotorType.kBrushless);
     private CANSparkMax hangMotor12 = new CANSparkMax(moterCanID12, MotorType.kBrushless);
 
     public Hanger()
     {
-        // //reset it every time -> to reset encoder specifically to initial settings
+        // //reset it every time -> to reset encosder specifically to initial settings
         hangMotor11.restoreFactoryDefaults();
         hangMotor12.restoreFactoryDefaults();
 
