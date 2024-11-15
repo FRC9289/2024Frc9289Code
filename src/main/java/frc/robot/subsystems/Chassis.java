b698f0d630d6d417a741cef62ae068941c84178e
eva <photogonical@gmail.com>
2024-11-15 16:07:13 -0600

    code changes

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.CommandConstants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -24,14 +25,14 @@ private CANSparkMax _backRight = new CANSparkMax(4, MotorType.kBrushed);
 
 
 //F -> Front, B -> Back, R -> Right, L -> Left, D -> Drive, T -> Turn
-private CANSparkMax FLDMotor = new CANSparkMax(1, MotorType.kBrushed); // FIELD
-private CANSparkMax FLTMotor = new CANSparkMax(2, MotorType.kBrushed); // FILLET
-private CANSparkMax FRDMotor = new CANSparkMax(3, MotorType.kBrushed); // FORD
-private CANSparkMax FRTMotor = new CANSparkMax(4, MotorType.kBrushed); // FART
-private CANSparkMax BLDMotor = new CANSparkMax(5, MotorType.kBrushed); // BLEED
-private CANSparkMax BLTMotor = new CANSparkMax(6, MotorType.kBrushed); // SANDWHICH
-private CANSparkMax BRDMotor = new CANSparkMax(7, MotorType.kBrushed); // BREAD
-private CANSparkMax BRTMotor = new CANSparkMax(8, MotorType.kBrushed); // brat
+private CANSparkMax FLDMotor = new CANSparkMax(1, MotorType.kBrushless); // FIELD
+private CANSparkMax FLTMotor = new CANSparkMax(2, MotorType.kBrushless); // FILLET
+private CANSparkMax FRDMotor = new CANSparkMax(3, MotorType.kBrushless); // FORD
+private CANSparkMax FRTMotor = new CANSparkMax(4, MotorType.kBrushless); // FART
+private CANSparkMax BLDMotor = new CANSparkMax(5, MotorType.kBrushless); // BLEED
+private CANSparkMax BLTMotor = new CANSparkMax(6, MotorType.kBrushless); // SANDWHICH
+private CANSparkMax BRDMotor = new CANSparkMax(7, MotorType.kBrushless); // BREAD
+private CANSparkMax BRTMotor = new CANSparkMax(8, MotorType.kBrushless); // brat
 
 private static final Chassis _chassis = new Chassis();
 
@@ -40,24 +41,34 @@ private DifferentialDrive _drive = new DifferentialDrive(_frontRight, _frontLeft
 
   public Chassis() 
   {
-    _backLeft.follow(_frontLeft);
-    _backRight.follow(_frontRight);
+    // _backLeft.follow(_frontLeft);
+    // _backRight.follow(_frontRight);
 
     // for motor correction
-    _frontLeft.setInverted(true);
-    _frontRight.setInverted(true);
-    _backLeft.setInverted(true);
-    _backRight.setInverted(true);
+    // _frontLeft.setInverted(true);
+    // _frontRight.setInverted(true);
+    // _backLeft.setInverted(true);
+    // _backRight.setInverted(true);
+
+    
   }
   
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("Front Left Speed", _frontLeft.get());
-    SmartDashboard.putNumber("Front Right Speed", _frontRight.get());
-    SmartDashboard.putNumber("Back Left Speed", _backLeft.get());
-    SmartDashboard.putNumber("Back Right Speed", _backRight.get());
+    // SmartDashboard.putNumber("Front Left Speed", _frontLeft.get());
+    // SmartDashboard.putNumber("Front Right Speed", _frontRight.get());
+    // SmartDashboard.putNumber("Back Left Speed", _backLeft.get());
+    // SmartDashboard.putNumber("Back Right Speed", _backRight.get());
+    SmartDashboard.putNumber("FLD", FLDMotor.get());
+    SmartDashboard.putNumber("FLT", FLTMotor.get());
+    SmartDashboard.putNumber("FRD", FRDMotor.get());
+    SmartDashboard.putNumber("FRT", FRTMotor.get());
+    SmartDashboard.putNumber("BLD", BLDMotor.get());
+    SmartDashboard.putNumber("BLT", BLTMotor.get());
+    SmartDashboard.putNumber("BRD", BRDMotor.get());
+    SmartDashboard.putNumber("BRT", BRTMotor.get());
 
     _drive.feed();
   }
@@ -78,12 +89,16 @@ private DifferentialDrive _drive = new DifferentialDrive(_frontRight, _frontLeft
 
   public void TeleOpDrive(Joystick controller)
   {
-    double axis1 = controller.getRawAxis(1);
-    double axis4 = controller.getRawAxis(4);
+    // double axis1 = controller.getRawAxis(1);
+    // double axis4 = controller.getRawAxis(4);
 
-    SmartDashboard.putNumber("axis 0", axis1);
-    SmartDashboard.putNumber("axis 4", axis4);
-    Drive(axis4, -axis1);
+    // SmartDashboard.putNumber("axis 0", axis1);
+    // SmartDashboard.putNumber("axis 4", axis4);
+    // Drive(axis4, -axis1);
+       double yAxis = controller.getRawAxis(CommandConstants.AxisLeftStickX);
+       double xAxis = controller.getRawAxis(CommandConstants.AxisLeftStickY);
+       double turn = controller.getRawAxis(CommandConstants.AxisRightStickX);
+       SwerveDrive.drive(yAxis, xAxis, turn);
   }
 
   public void autoDrive(double turnMovement, double linearMovement)
--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -1,12 +1,25 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
 public class SwerveDrive {
-    public final double L = 20; // equal to length between wheels axles
-    public final double W = 20; // equal to width between wheels axles
+    public static final double L = 20; // equal to length between wheels axles
+    public static final double W = 20; // equal to width between wheels axles
+
+    //F -> Front, B -> Back, R -> Right, L -> Left, D -> Drive, T -> Turn
+    private CANSparkMax FLDMotor = new CANSparkMax(1, MotorType.kBrushless); // FIELD
+    private CANSparkMax FLTMotor = new CANSparkMax(2, MotorType.kBrushless); // FILLET
+    private CANSparkMax FRDMotor = new CANSparkMax(3, MotorType.kBrushless); // FORD
+    private CANSparkMax FRTMotor = new CANSparkMax(4, MotorType.kBrushless); // FART
+    private CANSparkMax BLDMotor = new CANSparkMax(5, MotorType.kBrushless); // BLEED
+    private CANSparkMax BLTMotor = new CANSparkMax(6, MotorType.kBrushless); // SANDWHICH
+    private CANSparkMax BRDMotor = new CANSparkMax(7, MotorType.kBrushless); // BREAD
+    private CANSparkMax BRTMotor = new CANSparkMax(8, MotorType.kBrushless); // brat
 
     //x1 & y1 are the x and y axis from the strafing joystick
     // x2 is the x axis from the rotation joystick
-    public void drive(double x1, double y1, double x2){
+    public static void drive(double x1, double y1, double x2){
         // square root of the length + width squared
         double r = Math.sqrt ((L * L) + (W * W));
         //revsers the input of the y axis
@@ -30,5 +43,27 @@ public class SwerveDrive {
         double backLeftAngle = Math.atan2(a, c) / Math.PI;
         double frontRightAngle = Math.atan2(b, d) / Math.PI;
         double frontLeftAngle = Math.atan2(b, c) / Math.PI;
+    }
+    public SwerveDrive(){
+        // //reset it every time -> to reset encosder specifically to initial settings
+        FLDMotor.restoreFactoryDefaults();
+        FLTMotor.restoreFactoryDefaults();
+        FRDMotor.restoreFactoryDefaults();
+        FRTMotor.restoreFactoryDefaults();
+        BLDMotor.restoreFactoryDefaults();
+        BLTMotor.restoreFactoryDefaults();
+        BRDMotor.restoreFactoryDefaults();
+        BRTMotor.restoreFactoryDefaults();
+
+        // //set the speed limit on how fast the motor can go
+        hangMotor11.setSmartCurrentLimit(speedlimit);
+        hangMotor12.setSmartCurrentLimit(speedlimit);
+
+        // // shooterMotorB.follow(shooterMotorA);
+
+        // //saves changes to motor controller / encoder
+        hangMotor11.burnFlash();
+        hangMotor12.burnFlash();
+
     }
 }
